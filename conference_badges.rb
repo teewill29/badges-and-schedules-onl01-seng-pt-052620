a04b13fbb55f22eb7f8f297a06fa50dcb16db637
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 17:49:58 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,19 @@
-def badge_maker(name)
-  return "Hello, my name is #{name}."
-  
+def batch_badge_creator(names)
+  greetings = []  array
+  names.each do |name| 
+    greetings <<  badge_maker(name)
+  end
+  return greetings 
 end
 
-def batch_badge_creator(array)
-  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
-  attendees.each do |badges|
-    badges"Hello, my name is #{badges}."
+def assign_rooms(speakers)
+  greet = []
+  speakers.each_with_index{ |speakers, index| greet << "Hello, #{speakers}! You'll be assigned to room #{index+1}!"}
+  return greet
   end
-  return attendees
+
+def printer(inputOne)
+  batch_badge_creator(inputOne)
+  assign_rooms(inputOne)
+
 end
