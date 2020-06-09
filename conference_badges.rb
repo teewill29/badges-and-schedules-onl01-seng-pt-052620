78e5676ba67d3a43a44db4471d770ba708d3bc95
Tyler Williams <tyler.williams2996@gmail.com>
2020-06-09 17:49:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+  
+end
+
+def batch_badge_creator(array)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do |badges|
+    badges"Hello, my name is #{badges}."
+  end
+  return attendees
+end
